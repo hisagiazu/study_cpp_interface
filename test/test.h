41aaaf0d326cb207a7fcef3ba9b398474bf80dfe
Azusa Hisagi <azusa.hisagi@studio-kisetsu.net>
2014-02-23 11:48:44 +0900

    Refactaring project

--- test/test.h
+++ test/test.h
@@ -1,10 +1,21 @@
 #ifndef INCLUDED_TEST
 #define INCLUDED_TEST
 
-class CTest:public ITest
+#include <iostream>
+#include <string>
+
+#include "test_interface.h"
+
+using namespace std;
+
+class Test:public TestInterface
 {
 public:
-    void PrintTest();
+    Test(string str);
+    ~Test();
+    void Print();
+private:
+    string m_str;
 };
 
 #endif
